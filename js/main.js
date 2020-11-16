385525184fed0563b8220fd5b65c54ba0f2e3dd4
Fabrizio <whoet.wo@gmail.com>
2020-11-16 16:19:07 +0100

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const myApp = new Vue({
+   el:'#root',
+   data:{
+      message: 'Hello world!',
+      myImg: "https://lh3.googleusercontent.com/proxy/qnGWatoCaYbtA3DWyW-dReGkKlNIfhIoo-VJQBKfDTD_A4ZvyrmTy0clowM_26LRE-Ni4VEygwPRzdW8oNdtC3h8FlvSNyvJgfoKhMkFjkucQTYHbOhN"
+   }
+})
